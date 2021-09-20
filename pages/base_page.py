167a9c513393db256b35f118553db0b1570b7f9f
WildRider07 <sidvulk@gmail.com>
2021-09-20 17:46:49 +0300

    Base & main page

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+﻿from selenium import webdriver
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+    def open(self):
+        self.browser.get(self.url)
+main ="http://selenium1py.pythonanywhere.com/en-gb/catalogue/category/books_2/"
+oscar =  BasePage(webdriver.Chrome(), main)
+oscar.open()
