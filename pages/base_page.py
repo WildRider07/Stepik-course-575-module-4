d3bae3f7260123207f5e1b16ed150221baa6533d
WildRider07 <sidvulk@gmail.com>
2021-09-20 18:35:50 +0300

    updated test files according to 4.2 step 6

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,15 @@
 ﻿from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     def open(self):
         self.browser.get(self.url)
-main ="http://selenium1py.pythonanywhere.com/en-gb/catalogue/category/books_2/"
-oscar =  BasePage(webdriver.Chrome(), main)
-oscar.open()
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
