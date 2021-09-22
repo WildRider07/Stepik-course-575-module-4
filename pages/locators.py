51e68dd1ebf145f4b9ad2ef8c6edb8773fd22817
WildRider07 <sidvulk@gmail.com>
2021-09-22 13:36:41 +0300

    massive changes to base, main and tests

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
-
-
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
-class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ADD_MSG = (By.XPATH,"//strong[text() = 'Coders at Work']")
+    BASKET_MSG = (By.XPATH,"//p[contains (text(), 'basket')]/strong")
+    PRICE = (By.CSS_SELECTOR, "p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+#class LoginPageLocators():
+#    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+#    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
